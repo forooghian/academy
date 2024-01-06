1320240605f0cf4920016a380422473c7dd3c39f
Elyas Forooghian <e.forooghian@meedc.net>
2024-01-06 09:11:03 +0330

    Remove venv from repository

---
+++ academy/local_settings.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-0-k-!qp648n&3o*um&(#2%b94!4%s980v&)ag_-(b29(yrydhr'
--- academy/settings.py
+++ academy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from academy.local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0-k-!qp648n&3o*um&(#2%b94!4%s980v&)ag_-(b29(yrydhr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
