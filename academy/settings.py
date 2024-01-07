efdddd014fb8ecf373e387f2f8ef42e63c53f0fd
Elyas Forooghian <e.forooghian@meedc.net>
2024-01-07 09:24:15 +0330

    session2, blog and catalogue initialised!

--- academy/settings.py
+++ academy/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -35,6 +32,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blog.apps.BlogConfig',
+    'catalogue.apps.CatalogueConfig'
 ]
 
 MIDDLEWARE = [
---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import pathlib
+from blog.views import test
+
+urlpatterns = [
+    path('/list/',test),
+    path('/list/',test)
+]
